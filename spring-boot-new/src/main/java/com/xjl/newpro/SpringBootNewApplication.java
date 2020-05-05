7f445e06195b090f8c5f7ac22527cab696a047d9
xiusan <xiaojinlu1990@163.com>
2020-05-05 16:26:31 +0800

    升级springboot版本

---
+++ spring-boot-new/src/main/java/com/xjl/newpro/SpringBootNewApplication.java
@@ -0,0 +1,13 @@
+package com.xjl.newpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNewApplication.class, args);
+	}
+
+}
