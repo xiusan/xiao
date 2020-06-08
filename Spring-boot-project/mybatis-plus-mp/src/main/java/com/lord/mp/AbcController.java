da5912605600790e86b4404ab271aae226c42c49
xiusan <xiaojinlu1990@163.com>
2020-06-08 15:52:55 +0800

    MP自动代码初始化

---
+++ Spring-boot-project/mybatis-plus-mp/src/main/java/com/lord/mp/AbcController.java
@@ -0,0 +1,15 @@
+package com.lord.mp;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author xiaojinlu1990@163.com
+ * @date 2020/6/8 12:37
+ */
+@Controller
+public class AbcController {
+
+    public void jvm() {
+
+    }
+}
