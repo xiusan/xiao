edfaceab1f2adb6b23efe8c2c215c8df125a06bc
xiusan <xiaojinlu1990@163.com>
2020-06-18 22:25:27 +0800

    房价变动

---
+++ src/main/java/interview/nineteen/HaoWeilai/com/moretreand/partonelamdba/lombda/testentity/Bbb.java
@@ -0,0 +1,17 @@
+package interview.nineteen.HaoWeilai.com.moretreand.partonelamdba.lombda.testentity;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @Auther: xiaojinlu1990@163.com
+ * @Date: 2020/6/18 21:36
+ * @Description:
+ */
+@Data
+@Builder
+public class Bbb {
+    private String aaa;
+    private String bbb;
+    private String ccc;
+}
