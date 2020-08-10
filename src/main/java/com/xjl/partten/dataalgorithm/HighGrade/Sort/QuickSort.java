9dde58f52691acdb04cfe40c580dad0b945af1b0
xiusan <xiaojinlu1990@163.com>
2020-08-10 17:31:12 +0800

    快速排序

---
+++ src/main/java/com/xjl/partten/dataalgorithm/HighGrade/Sort/QuickSort.java
@@ -0,0 +1,10 @@
+package com.xjl.partten.dataalgorithm.HighGrade.Sort;
+
+/**
+ * 1 获取基准数
+ * 2 左右对比，对比交换，加入左边小于右边基准数放入
+ * 3 递归 直到完成
+ * Created by Administrator on 2020/8/10.
+ */
+public class QuickSort {
+}
